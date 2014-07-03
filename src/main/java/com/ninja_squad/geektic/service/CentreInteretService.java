fa0fcea3ad5bdc0e96b4b353702f94f047c72f77
Alexandre <ALEX@ALEX_PC>
2014-07-03 16:45:19 +0200

    ajout des classes pour les services

---
+++ src/main/java/com/ninja_squad/geektic/service/CentreInteretService.java
@@ -0,0 +1,28 @@
+package com.ninja_squad.geektic.service;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ninja_squad.geektic.dao.CentreInteretDao;
+import com.ninja_squad.geektic.modele.CentreInteret;
+
+@RestController
+@Transactional
+@RequestMapping("/afficheCentreInteret")
+public class CentreInteretService {
+	@Autowired
+    private CentreInteretDao CentreInteretDao;
+    
+    @RequestMapping(method = GET)
+    public List<CentreInteret> afficheInterets(){
+            List<CentreInteret> listeCentreInteret = CentreInteretDao.findByAll();
+            return listeCentreInteret;
+    }
+}
---
+++ src/main/java/com/ninja_squad/geektic/service/SexeGeekService.java
@@ -0,0 +1,28 @@
+package com.ninja_squad.geektic.service;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ninja_squad.geektic.modele.SexeGeek;
+import com.ninja_squad.geektic.dao.SexeGeekDao;
+
+@RestController
+@Transactional
+@RequestMapping("/afficheSexe")
+public class SexeGeekService {
+	@Autowired
+    private SexeGeekDao SexeGeekDao;
+    
+    @RequestMapping(method = GET)
+    public List<SexeGeek> afficheInterets(){
+            List<SexeGeek> listeCentreInteret = SexeGeekDao.findByAll();
+            return listeCentreInteret;
+    }
+}
