3ded19abc68a433b23a60f362443081882da77b1
Alexandre <ALEX@ALEX_PC>
2014-07-03 17:20:32 +0200

    maj du site

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,28 @@
+<script>
 var app = angular.module("geektic", ['ngRoute']);
 
 app.controller('HelloCtrl', function($scope, $http) {
     $http.get('/api/hello').success(function(helloMessage) {
         $scope.helloMessage = helloMessage;
     });
+    app.config(function($routeProvider)
+			{
+				$routeProvider.when('/', 
+				{
+					templateUrl: 'views/main.html',
+					controller: 'MainCtrl'
+				})
+				$routeProvider.when('/Info',
+				{
+					templateUrl: 'views/Infodetail.html',
+					controller: 'InfoCtrl'
+				})
+				$routeProvider.when('/Recherche',
+				{
+					templateUrl: 'views/resultrecherche.html',
+					controller: 'RechercheCtrl'
+				})
+				.otherwise('/');
+			}); 
 });
+</script>
