2244694d6a24b4e8508ffb17022ce6bc87e67921
Alexandre <ALEX@ALEX_PC>
2014-07-04 12:27:25 +0200

    version finale s Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/ninja_squad/geektic/service/GeeksCritereService.java
@@ -0,0 +1,29 @@
+package com.ninja_squad.geektic.service;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ninja_squad.geektic.dao.GeekDao;
+import com.ninja_squad.geektic.modele.Geek;
+
+@RestController
+@Transactional
+@RequestMapping("/api/afficheGeeksCritere")
+public class GeeksCritereService {
+	 @Autowired
+     private GeekDao geekDao;
+     
+     @RequestMapping(method = GET)
+     public List<Geek> afficheGeeks(@RequestParam String critere,@RequestParam String sexe){
+             List<Geek> listeGeeks = geekDao.findCritere(critere,sexe);
+             return listeGeeks;
+     }
+}
